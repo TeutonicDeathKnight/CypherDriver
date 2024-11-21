64179930002529daeee6e611bb9e0727561e1951
Zachary Seeley <zseeley7913@gmail.com>
2024-11-21 06:13:15 -0600

    Add project files.

---
+++ CypherDriver.cpp
@@ -0,0 +1,26 @@
+// CypherDriver.cpp -- Encrypte and decrypt text for use in communication
+//CSIS 212-<D02_202440>
+
+//Sources
+
+//include and using statements
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+//function prototypes
+
+int main()
+{
+
+	//Closing program statements
+	system("pause");
+	return 0;
+}
+
+//function definitions
+
